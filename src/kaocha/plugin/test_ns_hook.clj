a9663a1579a10d9033757994a1e589b885bfbc82
Philos Kim <philos99@gmail.com>
2023-12-22 19:28:55 +0900

    tesst-ns-hook drafted

--- src/kaocha/plugin/test_ns_hook.clj
+++ src/kaocha/plugin/test_ns_hook.clj
@@ -3,9 +3,51 @@
 
 (use 'debux.core)
 
+(defn- has-test-ns-hook? [ns]
+  (some (fn [[symbol var]]
+          (= symbol 'test-ns-hook))
+        (ns-publics ns)))
+
+(defn- remove-fixtures [ns-meta]
+  (dissoc ns-meta
+          :clojure.test/once-fixtures
+          :clojure.test/each-fixtures))
+
+(defn- create-test-case [ns]
+  (let [ns-str (-> ns ns-name name)
+        hook-var (ns-resolve ns 'test-ns-hook)
+        hook-meta (-> hook-var
+                      meta
+                      (dissoc :arglists)
+                      (assoc :test @hook-var))
+        hook-str "test-ns-str"]
+    {:kaocha.testable/type :kaocha.type/var,
+     :kaocha.testable/id (keyword ns-str hook-str)
+     :kaocha.testable/meta hook-meta
+     :kaocha.testable/desc hook-str
+     :kaocha.var/name (symbol ns-str hook-str)
+     :kaocha.var/var hook-var
+     :kaocha.var/test (:test hook-meta)
+     :kaocha.testable/wrap []}))
+
+(defn- update-test-suite
+  [{ns :kaocha.ns/ns :as test-suite}]
+  (if (has-test-ns-hook? ns)
+    (-> test-suite
+        (update :kaocha.testable/meta remove-fixtures)
+        (assoc :kaocha.test-plan/tests (list (create-test-case ns))))
+    test-suite))
+
+(defn- update-test-id [test-id]
+  (update test-id :kaocha.test-plan/tests
+          (fn [test-suites]
+            (map update-test-suite test-suites) )))
+
 (defmethod plugin/-register :kaocha.plugin/test-ns-hook
   [_name plugins]
   (conj plugins
         {:kaocha.hooks/pre-run
-         (fn [test-plan]
-           (dbg test-plan) )}))
+         (fn [config]
+           (update config :kaocha.test-plan/tests
+                   (fn [test-ids]
+                     (map update-test-id test-ids) )))}))
